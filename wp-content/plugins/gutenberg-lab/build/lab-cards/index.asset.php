84bd1be058faa59dce2ad2c234ee2c44f2278064
PRATHAMESH ANABHAVANE <prathameshanabhavane@PRATHAMESHs-MacBook-Air.local>
2026-08-08 21:47:37 +0530

    Style changes to flex

--- wp-content/plugins/gutenberg-lab/build/lab-cards/index.asset.php
+++ wp-content/plugins/gutenberg-lab/build/lab-cards/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '753bed15c81488d34c6b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ede362002f12cec67d6c');
