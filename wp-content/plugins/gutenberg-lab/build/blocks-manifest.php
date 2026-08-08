dbb33602254dc1ae826a23937e38c92be6fb3af8
PRATHAMESH ANABHAVANE <prathameshanabhavane@PRATHAMESHs-MacBook-Air.local>
2026-08-08 20:02:39 +0530

    Files added and simple gutenberg setup

---
+++ wp-content/plugins/gutenberg-lab/build/blocks-manifest.php
@@ -0,0 +1,22 @@
+<?php
+// This file is generated. Do not modify it manually.
+return array(
+	'build' => array(
+		'$schema' => 'https://schemas.wp.org/trunk/block.json',
+		'apiVersion' => 3,
+		'name' => 'create-block/gutenberg-lab',
+		'version' => '0.1.0',
+		'title' => 'Gutenberg Lab',
+		'category' => 'widgets',
+		'attributes' => array(
+			'message' => array(
+				'type' => 'string',
+				'default' => 'Hello Gutenberg'
+			)
+		),
+		'textdomain' => 'gutenberg-lab',
+		'editorScript' => 'file:./index.js',
+		'editorStyle' => 'file:./index.css',
+		'style' => 'file:./style-index.css'
+	)
+);
---
+++ wp-content/plugins/gutenberg-lab/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c78667380580c6175d91');
